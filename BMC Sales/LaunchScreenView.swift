4aba600aa5e44c0ef6bd85173c18e39940338ceb
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-11-06 21:05:45 +0300

    good LaunchScreen

---
+++ BMC Sales/LaunchScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreenView.swift
+//  BMC Sales
+//
+//  Created by Роман on 06.11.2023.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
