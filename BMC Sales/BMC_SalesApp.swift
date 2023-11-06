d51e0bddb096de2ed7a796d9de2e3854307a1c68
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-11-06 15:55:22 +0300

    Initial Commit

---
+++ BMC Sales/BMC_SalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMC_SalesApp.swift
+//  BMC Sales
+//
+//  Created by Роман on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BMC_SalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
